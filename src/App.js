44bdea4ed86ade7e307cedfc26e6e8d2f0b4f56c
sassi-montassar <sessi.montassar@gmail.com>
2021-02-03 16:37:57 +0100

    first jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import imagesrc from "./images/imageInSrc.jpg"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">sassi montassar</h1>
+   
+    <img src={imagesrc} alt="town2"/>
+   
+    <img src="/imageInPublic.jpg" alt="town1" />
+  </div>
+  <video width={320} height={240} controls autoPlay>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</>
     </div>
   );
 }
